3c484427998482b41189d9d1afe24c48d81747ab
Brendan Hay <brendan.g.hay@gmail.com>
2020-04-16 17:03:33 +0200

    Basic network state retrieval via point

--- haskell/urbit-azimuth/lib/Urbit/Azimuth.hs
+++ haskell/urbit-azimuth/lib/Urbit/Azimuth.hs
@@ -1,48 +1,9 @@
-module Urbit.Azimuth
-    (
+{-# LANGUAGE QuasiQuotes #-}
 
-    ) where
+{-# OPTIONS_GHC -fno-warn-missing-deriving-strategies #-}
 
--- import Numeric.Natural (Natural)
+module Urbit.Azimuth where
 
-import qualified Urbit.Ob
-import qualified Network.Ethereum.Api.Types as Ethereum.Types
-import qualified Urbit.Azimuth.Contract as Contract
+import qualified Network.Ethereum.Contract.TH as Ethereum.Contract
 
-newtype Address = Address Natural
-    deriving stock (Show, Read, Eq, Ord, Num, Real, Integral)
-
-data Point = Point
-    { encryptionKey     :: Int
-    , authenticationKey :: Int
-    , hasSponsor        :: Int
-    , active            :: Int
-    , escapeRequested   :: Int
-    , sponsor           :: Int
-    , escapeTo          :: Int
-    , cryptoSuite       :: Int
-    , keyRevision       :: Int
-    , continuityNum     :: Int
-    } deriving stock (Show, Eq)
-
--- data EthPoint = EthPoint
---     { epOwn :: (EthAddr, EthAddr, EthAddr, EthAddr)
---     , epNet :: Maybe (Life, Pass, ContNum, (Bool, Ship), Maybe Ship)
---     , epKid :: Maybe (EthAddr, HoonSet Ship)
---     }
---   deriving (Eq, Show)
-
-getPoint point = do
-    ( encryptionKey
-    , authenticationKey
-    , hasSponsor
-    , active
-    , escapeRequested
-    , sponsor
-    , escapeTo
-    , cryptoSuite
-    , keyRevision
-    , continuityNum
-    ) <- Contract.points point
-
-    pure Point{..}
+[Ethereum.Contract.abiFrom|azimuth.json|]
