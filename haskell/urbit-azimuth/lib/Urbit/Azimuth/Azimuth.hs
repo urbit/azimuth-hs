56fb7ad064fd4215b48e9280e73a4397d7b70c17
Brendan Hay <brendan.g.hay@gmail.com>
2020-05-18 14:58:21 +0200

    Adding support for ecliptic conigureKeys

---
+++ haskell/urbit-azimuth/lib/Urbit/Azimuth/Azimuth.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+{-# OPTIONS_GHC -fno-warn-missing-deriving-strategies #-}
+{-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
+{-# OPTIONS_GHC -fno-warn-missing-export-lists #-}
+
+module Urbit.Azimuth.Azimuth where
+
+-- import qualified Paths_urbit_azimuth
+import qualified Network.Ethereum.Contract.TH as Ethereum.Contract
+
+[Ethereum.Contract.abiFrom|azimuth.json|]
+-- $(Paths_urbit_azimuth.getDataFileName "azimuth.json")
