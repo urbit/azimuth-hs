4007bc557c3b15e3d8c289a226c684b45f423346
Jared Tobin <jared@jtobin.io>
2020-07-10 11:13:56 +0400

    Add multiple test accounts.

--- test/Main.hs
+++ test/Main.hs
@@ -2,78 +2,77 @@
 
 module Main where
 
-import qualified Data.ByteString as BS
 import Control.Exception (bracket)
-import Crypto.Secp256k1
 import qualified Data.Text as T
 import qualified Shelly as S
 import Test.Hspec
 import qualified Urbit.Azimuth as A
 import qualified Urbit.Ob as Ob
-import Haskoin.Keys
 
-initGanache :: IO T.Text
-initGanache = S.shelly $ S.verbosely $
+ginit :: IO T.Text
+ginit = S.shelly $ S.verbosely $
   S.run "docker" [
       "run"
     , "-d"
-    , "-p"
-    , "8545:8545"
+    , "-p", "8545:8545"
     , "trufflesuite/ganache-cli:latest"
-    , "--networkId"
-    , "1"
-    , "-m"
-    , "'benefit crew supreme gesture quantum web media hazard " <>
-      "theory mercy wing kitten'"
+    , "--networkId", "1"
+    , "-m", "'benefit crew supreme gesture quantum web media hazard " <>
+            "theory mercy wing kitten'"
     ]
 
-killGanache :: T.Text -> IO ()
-killGanache pod = S.shelly $ S.verbosely $ do
-  _ <- S.run "docker" [
+gkill :: T.Text -> IO ()
+gkill pod = S.shelly $ S.verbosely $
+  S.run_ "docker" [
       "stop"
     , pod
     ]
 
-  pure ()
-
 withGanache :: IO () -> IO ()
-withGanache action = bracket initGanache killGanache (const action)
+withGanache action = bracket ginit gkill (const action)
 
-mnem :: T.Text
-mnem = "benefit crew supreme gesture quantum web media hazard "
-    <> "theory mercy wing kitten"
+mnem :: A.Mnemonic
+mnem =
+     "benefit crew supreme gesture quantum web media hazard theory mercy wing "
+  <> "kitten"
 
-seed :: Either String Seed
-seed = mnemonicToSeed mempty mnem
+pbase :: A.DerivPath
+pbase = A.Deriv A.:| 44 A.:| 60 A.:| 0 A.:/ 0
 
-hd = case seed of
-  Left _  -> error "bang"
-  Right x -> makeXPrvKey x
+path0 :: A.DerivPath
+path0 = pbase A.:/ 0
 
-path :: DerivPathI AnyDeriv
-path = Deriv :| 44 :| 60 :| 0 :/ 0
+path1 :: A.DerivPath
+path1 = pbase A.:/ 1
+
+path2 :: A.DerivPath
+path2 = pbase A.:/ 2
 
 pk0 :: A.LocalKey
-pk0 = A.LocalKey key 0
-  where
-    key =
-        A.importKey
-      . getSecKey
-      . xPrvKey
-      . derivePath (path :/ 0)
-      $ hd
+pk0 = case A.getLocalKey mnem mempty path0 of
+  Right x -> x
+  _       -> error "bang"
 
-simple :: A.LocalKey -> Ob.Patp -> IO A.Point
+
+simple
+  :: (A.JsonRpc m, MonadFail m)
+  => A.LocalKey
+  -> Ob.Patp
+  -> m A.Point
 simple acct patp = do
-  client <- A.defaultSettings "http://localhost:8545"
-  A.runWeb3 client $ do
-    block <- A.blockNumber
-    A.withAccount acct $ do
-      contracts <- A.getContracts
-      A.runAzimuth contracts block (A.getPoint patp)
+  block <- A.blockNumber
+  A.withAccount acct $ do
+    contracts <- A.getContracts
+    A.runAzimuth contracts block (A.getPoint patp)
 
 main :: IO ()
-main = hspec $ around_ withGanache $
-  it "does something" $ do
-    point <- simple pk0 (Ob.patp 0)
-    print point
+main = do
+  client <- A.defaultSettings "http://localhost:8545"
+
+  hspec $ around_ withGanache $
+    it "does something" $ do
+      point <- A.runWeb3 client $ do
+        block <- A.blockNumber
+        simple pk0 (Ob.patp 0)
+
+      print point
